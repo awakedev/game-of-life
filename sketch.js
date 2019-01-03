49725abfadb0d0cbadf43ef27b3ca95175090cc7
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-03 15:16:22 +0000

    2D Array Creation

--- sketch.js
+++ sketch.js
@@ -1,12 +1,46 @@
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+
+    }
+    return arr;
+
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 40;
 
 
 function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(255, 0, 200);
-      }
+    createCanvas(600, 600);
+    cols = width / resolution;
+    rows = width / resolution;
 
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+
+
+        }
+    }
+}
 
 function draw() {
     background(0);
 
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(255);
+                stroke(255);
+                rect(x, y, resolution, resolution);
+            }
         }
+    }
+}
